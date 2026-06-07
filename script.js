93c684dda6d5a5d9a89cf4464859cdf2e4d14af3
tickri <126425117+tickri@users.noreply.github.com>
2026-06-07 23:38:20 +0200

    Createt function getComputerChoice returning "rock", "paper" or "scissors" randomly

---
+++ script.js
@@ -0,0 +1,14 @@
+// Step 2 logic to getComputerChoice
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+console.log(getComputerChoice());
