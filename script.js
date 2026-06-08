37eb991f09c1fbcb491f8c2079c85c71d834a8aa
tickri <126425117+tickri@users.noreply.github.com>
2026-06-08 23:58:53 +0200

    Added function to play entire game

--- script.js
+++ script.js
@@ -18,32 +18,50 @@ function getHumanChoice() {
   return prompt("Enter Rock, Paper or Scissors").toLowerCase();
 }
 
-// Declaring Value 0 to humanScore and computerScore
+// Function to play entire game
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  // Declaring Value 0 to humanScore and computerScore
 
-// Function to play one single round
+  let humanScore = 0;
+  let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie! Both chose " + humanChoice);
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log("You win! " + humanChoice + " beats " + computerChoice);
-    humanScore++;
-  } else {
-    console.log("You lose! " + computerChoice + " beats " + humanChoice);
-    computerScore++;
+  // Function to play one single round
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log("It's a tie! Both chose " + humanChoice);
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("You win! " + humanChoice + " beats " + computerChoice);
+      humanScore++;
+    } else {
+      console.log("You lose! " + computerChoice + " beats " + humanChoice);
+      computerScore++;
+    }
+    console.log("your score: " + humanScore);
+    console.log("computers score: " + computerScore);
   }
+
+  const humanChoice = getHumanChoice();
+  const computerChoice = getComputerChoice();
+
+  // Round 1
+  playRound(getHumanChoice(), getComputerChoice());
+  // Round 2
+  playRound(getHumanChoice(), getComputerChoice());
+  // Round 3
+  playRound(getHumanChoice(), getComputerChoice());
+  // Round 4
+  playRound(getHumanChoice(), getComputerChoice());
+  // Round 5
+  playRound(getHumanChoice(), getComputerChoice());
+
   console.log("your score: " + humanScore);
   console.log("computers score: " + computerScore);
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
