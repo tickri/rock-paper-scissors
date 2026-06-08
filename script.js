ea82bc53aca1d434bdae8e9741e5e4a02712b21d
tickri <126425117+tickri@users.noreply.github.com>
2026-06-08 20:18:34 +0200

    Added function to get user choice all lower case

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-// Step 2 logic to getComputerChoice
+// Random Computer Choice
 
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random()*3);
+  const randomNumber = Math.floor(Math.random() * 3);
 
-    if (randomNumber === 0) {
-        return "rock";
-    } else if (randomNumber === 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 console.log(getComputerChoice());
+
+// User Choice all lower case
+
+function getHumanchoice() {
+  return prompt("Enter Rock, Paper or Scissors").toLowerCase();
+}
+console.log(getHumanchoice());
