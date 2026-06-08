d9ede88aeaf3104990f93c70242f5a712b8b0cd6
tickri <126425117+tickri@users.noreply.github.com>
2026-06-08 23:37:52 +0200

    Created a function to play one round

--- script.js
+++ script.js
@@ -11,11 +11,39 @@ function getComputerChoice() {
     return "scissors";
   }
 }
-console.log(getComputerChoice());
 
 // User Choice all lower case
 
-function getHumanchoice() {
+function getHumanChoice() {
   return prompt("Enter Rock, Paper or Scissors").toLowerCase();
 }
-console.log(getHumanchoice());
+
+// Declaring Value 0 to humanScore and computerScore
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Function to play one single round
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! Both chose " + humanChoice);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win! " + humanChoice + " beats " + computerChoice);
+    humanScore++;
+  } else {
+    console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    computerScore++;
+  }
+  console.log("your score: " + humanScore);
+  console.log("computers score: " + computerScore);
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
